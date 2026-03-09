f259dafe21c0a12005a2afedf5a9faec9eace87e
87-Nayanika <2305464@kiit.ac.in>
2026-03-09 10:16:45 +0530

    Refactor search algorithms and add run functions

--- script.js
+++ script.js
@@ -49,13 +49,14 @@ function valid(x,y){
 if(x<0 || y<0 || x>=8 || y>=8) return false;
 
 for(let o of obstacles){
-
 if(o[0]==x && o[1]==y) return false;
-
 }
 
 return true;
+
 }
+
+/* BFS */
 
 function bfs(){
 
@@ -68,13 +69,10 @@ let [pos,path]=queue.shift();
 let key=pos.toString();
 
 if(visited.has(key)) continue;
-
 visited.add(key);
 
 if(pos[0]==goal[0] && pos[1]==goal[1]){
-
 return path;
-
 }
 
 for(let m of moves){
@@ -83,9 +81,7 @@ let nx=pos[0]+m[0];
 let ny=pos[1]+m[1];
 
 if(valid(nx,ny)){
-
 queue.push([[nx,ny],[...path,[nx,ny]]]);
-
 }
 
 }
@@ -95,6 +91,126 @@ queue.push([[nx,ny],[...path,[nx,ny]]]);
 return null;
 
 }
+
+/* DFS */
+
+function dfs(){
+
+let stack=[[start,[start]]];
+let visited=new Set();
+
+while(stack.length){
+
+let [pos,path]=stack.pop();
+let key=pos.toString();
+
+if(visited.has(key)) continue;
+visited.add(key);
+
+if(pos[0]==goal[0] && pos[1]==goal[1]){
+return path;
+}
+
+for(let m of moves){
+
+let nx=pos[0]+m[0];
+let ny=pos[1]+m[1];
+
+if(valid(nx,ny)){
+stack.push([[nx,ny],[...path,[nx,ny]]]);
+}
+
+}
+
+}
+
+return null;
+
+}
+
+/* Heuristic */
+
+function heuristic(x,y){
+return Math.abs(x-goal[0]) + Math.abs(y-goal[1]);
+}
+
+/* Greedy Search */
+
+function greedy(){
+
+let open=[[start,[start]]];
+let visited=new Set();
+
+while(open.length){
+
+open.sort((a,b)=>heuristic(a[0][0],a[0][1]) - heuristic(b[0][0],b[0][1]));
+
+let [pos,path]=open.shift();
+let key=pos.toString();
+
+if(visited.has(key)) continue;
+visited.add(key);
+
+if(pos[0]==goal[0] && pos[1]==goal[1]){
+return path;
+}
+
+for(let m of moves){
+
+let nx=pos[0]+m[0];
+let ny=pos[1]+m[1];
+
+if(valid(nx,ny)){
+open.push([[nx,ny],[...path,[nx,ny]]]);
+}
+
+}
+
+}
+
+return null;
+
+}
+
+/* A* Search */
+
+function aStar(){
+
+let open=[[start,[start],0]];
+let visited=new Set();
+
+while(open.length){
+
+open.sort((a,b)=>(a[2]+heuristic(a[0][0],a[0][1]))-(b[2]+heuristic(b[0][0],b[0][1])));
+
+let [pos,path,cost]=open.shift();
+let key=pos.toString();
+
+if(visited.has(key)) continue;
+visited.add(key);
+
+if(pos[0]==goal[0] && pos[1]==goal[1]){
+return path;
+}
+
+for(let m of moves){
+
+let nx=pos[0]+m[0];
+let ny=pos[1]+m[1];
+
+if(valid(nx,ny)){
+open.push([[nx,ny],[...path,[nx,ny]],cost+1]);
+}
+
+}
+
+}
+
+return null;
+
+}
+
+/* Draw Path */
 
 function drawPath(path){
 
@@ -121,11 +237,26 @@ ctx.stroke();
 
 }
 
-function runBFS(){
+/* Run Functions */
 
+function runBFS(){
 let path=bfs();
 drawPath(path);
+}
 
+function runDFS(){
+let path=dfs();
+drawPath(path);
+}
+
+function runGreedy(){
+let path=greedy();
+drawPath(path);
+}
+
+function runAStar(){
+let path=aStar();
+drawPath(path);
 }
 
 drawBoard();
